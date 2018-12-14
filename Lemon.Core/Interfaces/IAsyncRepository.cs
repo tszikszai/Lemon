9a06fa4b7dcce4dcb0070de79279ac79e43fa16a
Tibor Szikszai <szikszai.tibor@gmail.com>
2018-12-14 16:41:43 +0200

    implement repository and async repository interfaces

---
+++ Lemon.Core/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using Lemon.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemon.Core.Interfaces
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ Lemon.Core/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Lemon.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lemon.Core.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
