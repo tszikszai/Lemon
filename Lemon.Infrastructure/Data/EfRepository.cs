3d8199448e4a7292da68b845303bb9f57fae048a
Tibor Szikszai <szikszai.tibor@gmail.com>
2018-12-14 16:42:01 +0200

    implement EF repository class

---
+++ Lemon.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,79 @@
+﻿using Lemon.Core.Entities;
+using Lemon.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemon.Infrastructure.Data
+{
+    public class EfRepository<T> : IRepository<T>, IAsyncRepository<T> where T : BaseEntity
+    {
+        private readonly LemonContext _dbContext;
+
+        public EfRepository(LemonContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public T GetById(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsEnumerable();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
