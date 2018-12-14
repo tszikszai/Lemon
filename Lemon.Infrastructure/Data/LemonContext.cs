3fac3fa562d3999e8b80baf09f00e69ec4aba474
Tibor Szikszai <szikszai.tibor@gmail.com>
2018-12-14 12:29:46 +0200

    implement LemonContext class

---
+++ Lemon.Infrastructure/Data/LemonContext.cs
@@ -0,0 +1,27 @@
+﻿using Lemon.Core.Entities.Artists;
+using Lemon.Infrastructure.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lemon.Infrastructure.Data
+{
+    public class LemonContext : DbContext
+    {
+        public LemonContext(DbContextOptions<LemonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Musician> Musicians { get; set; }
+        public DbSet<Band> Bands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArtistConfiguration());
+            modelBuilder.ApplyConfiguration(new MusicianConfiguration());
+            modelBuilder.ApplyConfiguration(new BandConfiguration());
+        }
+    }
+}
