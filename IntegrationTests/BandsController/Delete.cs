ef592f38cfb0c7aa88b2a5c906ffca20beeda110
Tibor Szikszai <szikszai.tibor@gmail.com>
2019-04-17 15:39:32 +0300

    implement integration tests for the delete web API operations

---
+++ IntegrationTests/BandsController/Delete.cs
@@ -0,0 +1,52 @@
+﻿using Lemon.Web;
+using Lemon.Web.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests.BandsController
+{
+    public class Delete : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private const string BaseUrl = "/api/bands";
+
+        private readonly HttpClient _client;
+
+        public Delete(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsNotFoundForInvalidId()
+        {
+            HttpResponseMessage response = await _client.DeleteAsync($"{BaseUrl}/0");
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            Assert.Equal("0", responseContent);
+        }
+
+        [Fact]
+        public async Task ReturnsNoContentForValidId()
+        {
+            HttpResponseMessage response = await _client.GetAsync($"{BaseUrl}");
+            response.EnsureSuccessStatusCode();
+            string responseContent = await response.Content.ReadAsStringAsync();
+            IEnumerable<BandViewModel> bands = JsonConvert.DeserializeObject<IEnumerable<BandViewModel>>(responseContent);
+            int idToDelete = bands.First().Id;
+
+            response = await _client.DeleteAsync($"{BaseUrl}/{idToDelete}");
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+            response = await _client.GetAsync($"{BaseUrl}/{idToDelete}");
+            responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            Assert.Equal(idToDelete.ToString(), responseContent);
+        }
+    }
+}
---
+++ IntegrationTests/Utilities.cs
@@ -0,0 +1,67 @@
+﻿using Lemon.Core.Entities.Artists;
+using Lemon.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public static class Utilities
+    {
+        public static void InitializeDbForTests(LemonContext db)
+        {
+            db.Bands.AddRange(GetBands());
+            db.Musicians.AddRange(GetMusicians());
+            db.SaveChanges();
+        }
+
+        public static List<Band> GetBands()
+        {
+            return new List<Band>
+            {
+                new Band
+                {
+                    Name = "Radiohead",
+                    ActiveFromYear = 1985
+                },
+                new Band
+                {
+                    Name = "Tool",
+                    ActiveFromYear = 1990
+                },
+                new Band
+                {
+                    Name = "The Doors",
+                    ActiveFromYear = 1965,
+                    ActiveToYear = 1973
+                }
+            };
+        }
+
+        public static List<Musician> GetMusicians()
+        {
+            return new List<Musician>
+            {
+                new Musician
+                {
+                    FirstName = "Thom",
+                    LastName = "Yorke",
+                    DateOfBirth = new DateTime(1968, 10, 7)
+                },
+                new Musician
+                {
+                    FirstName = "Maynard James",
+                    LastName = "Keenan",
+                    DateOfBirth = new DateTime(1964, 4, 17)
+                },
+                new Musician
+                {
+                    FirstName = "Jim",
+                    LastName = "Morrison",
+                    DateOfBirth = new DateTime(1943, 12, 8)
+                }
+            };
+        }
+    }
+}
