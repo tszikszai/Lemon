016f2b5ff21431c2fc795a7f11628ef9cb1c4f77
Tibor Szikszai <szikszai.tibor@gmail.com>
2019-04-18 12:47:19 +0300

    implement integration tests for the get all web API operations

---
+++ IntegrationTests/BandsController/GetAll.cs
@@ -0,0 +1,39 @@
+﻿using Lemon.Web;
+using Lemon.Web.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests.BandsController
+{
+    public class GetAll : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private const string BaseUrl = "/api/bands";
+
+        private readonly HttpClient _client;
+
+        public GetAll(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsAllBands()
+        {
+            HttpResponseMessage response = await _client.GetAsync($"{BaseUrl}");
+            response.EnsureSuccessStatusCode();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            IEnumerable<BandViewModel> bands = JsonConvert.DeserializeObject<IEnumerable<BandViewModel>>(responseContent);
+
+            Assert.Equal(3, bands.Count());
+            Assert.Contains(bands, x => x.Name == "Radiohead");
+            Assert.Contains(bands, x => x.Name == "Tool");
+            Assert.Contains(bands, x => x.Name == "The Doors");
+        }
+    }
+}
---
+++ IntegrationTests/MusiciansController/GetAll.cs
@@ -0,0 +1,39 @@
+﻿using Lemon.Web;
+using Lemon.Web.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests.MusiciansController
+{
+    public class GetAll : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private const string BaseUrl = "/api/musicians";
+
+        private readonly HttpClient _client;
+
+        public GetAll(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsAllAuthors()
+        {
+            HttpResponseMessage response = await _client.GetAsync($"{BaseUrl}");
+            response.EnsureSuccessStatusCode();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            IEnumerable<MusicianViewModel> musicians = JsonConvert.DeserializeObject<IEnumerable<MusicianViewModel>>(responseContent);
+
+            Assert.Equal(3, musicians.Count());
+            Assert.Contains(musicians, x => x.FirstName == "Thom" && x.LastName == "Yorke");
+            Assert.Contains(musicians, x => x.FirstName == "Maynard James" && x.LastName == "Keenan");
+            Assert.Contains(musicians, x => x.FirstName == "Jim" && x.LastName == "Morrison");
+        }
+    }
+}
